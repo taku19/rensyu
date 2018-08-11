5ab95842d1479ae80caf24f5c8dbc9c12b96beda
Takuto Ohara <taku@takunoMacBook-Pro.local>
2018-08-11 22:54:47 +0900

    some coding

--- tetris/GameViewController.swift
+++ tetris/GameViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
+    var scene: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Configure the view.
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configyre the scene.
+        scene = GameScene(size : skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // Present the Scene.
+        skView.presentScene(scene)
+        
     }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }

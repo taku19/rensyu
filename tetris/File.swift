9caeddd887b2f498681c73e954411d0b69a1fb1b
Takuto Ohara <taku@takunoMacBook-Pro.local>
2018-08-12 03:04:18 +0900

    more coding

---
+++ tetris/File.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  tetris
+//
+//  Created by taku on 2018/08/12.
+//  Copyright © 2018年 alche. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    // #2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        // #3
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+        
+    }
+
+// #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+}

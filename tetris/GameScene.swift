861963eea1ba2f229d826e2c50b8358662fbe84f
Takuto Ohara <taku@takunoMacBook-Pro.local>
2018-08-12 03:14:00 +0900

    more coding

--- tetris/GameScene.swift
+++ tetris/GameScene.swift
@@ -8,7 +8,13 @@
 
 import SpriteKit
 
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:NSDate?
+    
     required init(coder aDecoder: NSCoder)  {
         fatalError("NSCoder not suppoted")
     }
@@ -23,7 +29,24 @@ class GameScene: SKScene {
         background.anchorPoint = CGPoint(x: 0, y:  1.0)
         addChild(background)
     }
-       override func update(currentTime: CFTimeInterval) {
+    
+    override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        guard let lastTick = lastTick else {
+            return
+        }
+        let timePassed = lastTick.timeIntervalSinceNow * -1000.0
+        if timePassed > tickLengthMillis {
+            self.lastTick = NSDate()
+            tick?()
+        }
+    }
+    
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = Nil
     }
 }
